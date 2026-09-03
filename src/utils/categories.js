ce531161ff7f1a5bcbb53f44551d76dee0a6b2bb
azeefashraf22sketch <azeefashraf22@gmail.com>
2026-09-03 11:01:26 -0700

    feat: add categories data structure for service-related professions

---
+++ src/utils/categories.js
@@ -0,0 +1,49 @@
+export const CATEGORIES = [
+    {
+        name: 'Electrician',
+        value: 'Electrical',
+        icon: '⚡',
+        color: '#FFB020'
+    },
+    {
+        name: 'Plumber',
+        value: 'Plumbing',
+        icon: '🔧', color: '#47BFFF'
+
+    },
+    {
+        name: 'Cleaner',
+        value: 'Cleaning',
+        icon: '🧹', color: '#0F9B8E'
+
+    },
+    {
+        name: 'Painter',
+        value: 'Painting',
+        icon: '🎨',
+        color: '#F0553F'
+    },
+    {
+        name: 'Carpenter',
+        value: 'Carpentry',
+        icon: '🪚',
+        color: '#8900FF'
+    },
+    {
+        name: 'Tutor', value: 'Tutoring',
+        icon: '📚', color: '#2B2F79'
+
+    },
+    {
+        name: 'Appliance Repair', value: 'Appliance Repair',
+        icon: '🔌',
+        color: '#FF7A59'
+
+    },
+    {
+        name: 'Gardener', value: 'Gardening',
+        icon: '🌿',
+        color: '#3BB273'
+    },
+
+];
